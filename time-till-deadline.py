684a2f7d82a9d2ba27ec0d9b0548f8b678858c68
Centli Allan Garcés Buendia <yosoycentli@gmail.com>
2023-11-28 09:56:29 -0600

    [ADD] functionality to see the deadline in days and hours

--- time-till-deadline.py
+++ time-till-deadline.py
@@ -1,3 +1,16 @@
 import datetime
 
-input("Enter your goal with deadline separated by colon\n")
+user_input = input("Enter your goal with deadline separated by colon (DD.MM.YYYY)\n")
+input_list = user_input.split(":")
+
+goal = input_list[0]
+deadline = input_list[1]
+
+deadline_date = datetime.datetime.strptime(deadline, "%d.%m.%Y")
+today_date = datetime.datetime.today()
+#calculate how many days from now till deadline
+time_till = deadline_date - today_date
+
+hours_till = int(time_till.total_seconds() / 60 / 60)
+
+print(f"Dear user! Time remaning for your goal: {goal} is {time_till.days} days or {hours_till} hours.")
