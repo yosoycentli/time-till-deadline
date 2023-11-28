eeca3f86b338f743e3f7aad6fec3c8b17ed87ea3
Centli Allan Garcés Buendia <yosoycentli@gmail.com>
2023-11-28 10:00:27 -0600

    [UPDATE] reduce code and optimize datetime package use

--- time-till-deadline.py
+++ time-till-deadline.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 
 user_input = input("Enter your goal with deadline separated by colon (DD.MM.YYYY)\n")
 input_list = user_input.split(":")
@@ -6,8 +6,8 @@ input_list = user_input.split(":")
 goal = input_list[0]
 deadline = input_list[1]
 
-deadline_date = datetime.datetime.strptime(deadline, "%d.%m.%Y")
-today_date = datetime.datetime.today()
+deadline_date = datetime.strptime(deadline, "%d.%m.%Y")
+today_date = datetime.today()
 #calculate how many days from now till deadline
 time_till = deadline_date - today_date
 
